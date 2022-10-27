a056d3351323ee1960e91d41a9b6917f326f2478
Crtmetton <crtmetton@gmail.com>
2022-10-27 21:42:02 +0200

    Update

--- slvs/includer/var-collection.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-#common var (ion-icons)
-
-    $gear = '<ion-icon name="cog-outline"></ion-icon>';
-    $more = '<ion-icon name="add-outline"></ion-icon>';
-    $close = '<ion-icon name="close-outline"></ion-icon>';
-    $save = '<ion-icon name="save-outline"></ion-icon>';
-?>
