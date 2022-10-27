28d9343ea0627be58151374b3bfec52dda603921
Crtmetton <crtmetton@gmail.com>
2022-10-27 22:35:09 +0200

    D

--- slvs/includer/nav_allpage.php
+++ slvs/includer/nav_allpage.php
@@ -1,6 +1,3 @@
-// <?php 
-// include("session.php"); 
-// ?>
 <haed>
     <link rel="stylesheet" href="../css/includercss/nav_allpage.css">
 </haed>
@@ -10,10 +7,6 @@
         <a class="pages_nav_a" href="portfolio"><div class="navportfolio"><p class="nav_btn">Portfolio</p></div></a>
         <!-- <a class="pages_nav_a" href="sst"><div class="navsst"><p class="nav_btn">Silvus Stream Tech</p></div></a> -->
         <a class="pages_nav_a" href="cv"><div class="navcv"><p class="nav_btn">CV</p></div></a>
-        // <?php 
-        // if(isset($_SESSION['user'])){
-        //     echo'<a href="../account/disconnect.php"><div class="connect"></div></a>';
-        // }
-        // ?>
+
     </div>  
 </header>
